139a539e30429493d4bb18f6d6a0d10cb962083c
Danilo Xavier <daniloxaviergo@gmail.com>
2021-10-05 11:01:06 -0300

    using file from reload

--- wa.sh
+++ wa.sh
@@ -1,49 +1,22 @@
 #!/bin/bash
 
-# janelas do primeiro monitor workspace atual
-
-screen=3840
-topscreen=414
-
-if [ "$1" == "" ]; then
-  cw=$(wmctrl -d | sed -n 's/^\([0-9]\+\) *\*.*/\1/p') # current workspace
-else
-  cw=$1
-fi
-
-wid=''
-lines=''
 while read -r line ; do
-  width=$(echo $line | awk '{ print $3 }')
-  top=$(echo $line | awk '{ print $4 }')
-  workspace=$(echo $line | awk '{ print $2 }')
-  if [ "$width" -lt "$screen" ] && [ "$width" -gt -9000 ] && [ "$top" -gt "$topscreen" ]
+  workspace_monitor=$(echo $line | awk '{print $NF}' | grep 'm1')
+  if [ "$workspace_monitor" ]
   then
-    if [ "$workspace" = "$cw" ] || [ "$cw" = "all" ]
-    then
-      if [ -z "$wid" ]
-      then
-        linne=$(echo $line | sed 's/[\]//g' | awk '{ s = ""; for (i = 9; i <= NF; i++) s = s $i " "; print $1, $7, s }')
-        windowid=$(echo $line | awk '{ print $1 }')
-        wwworkspace=$(echo $line | awk '{ print $2 }')
-        title=$(echo $line | sed 's/[\]//g' | awk '{ s = ""; for (i = 8; i <= NF; i++) s = s $i " "; print $7, s }')
-        linne="$windowid k$((wwworkspace+1)) $title"
-        lines="$linne\n$lines"
-
-        # old
-        # line=$(echo $line | sed 's/[\]//g' | awk '{ s = ""; for (i = 9; i <= NF; i++) s = s $i " "; print $1, $7, s }')
-        # lines="$line\n$lines"
-      fi
-    fi
+    lines="$line\n$lines"
   fi
-done < <(wmctrl -dliGx)
+done < <(cat /home/danilo/scripts/dmenu/list_windows)
 
 choice=$(echo -e "$lines" | awk '{ s = ""; for (i = 2; i <= NF; i++) s = s $i " "; print s }' | dmenu -i -l 20 -b -m 2 -fn Monospace-16:normal)
 if [ "$choice" == "" ]; then
   exit
 fi
 
+choice=$(echo $choice | tr -d '\n')
 wid=$(echo -e "$lines" | grep "$choice" | awk '{ print $1 }')
-wworkspace=$(wmctrl -dliGx | grep "$wid" | awk '{ print $2 }')
-wmctrl -s $wworkspace && sleep 0.01s
+monitor_workspace=$(echo -e "$lines" | grep "$choice" | awk '{ print $NF }')
+workspace=$(expr ${monitor_workspace:1:1} - 1)
+
+wmctrl -s $workspace && sleep 0.01s
 wmctrl -i -a $wid
--- wd.sh
+++ wd.sh
@@ -1,58 +1,22 @@
 #!/bin/bash
 
-# janelas do terceiro monitor workspace atual
-
-bscreen=7679 # begin screen
-escreen=9178 # end screen
-topscreen=1300 # top porque tem monitor no alto
-
-
-if [ "$1" == "" ]; then
-  cw=$(wmctrl -d | sed -n 's/^\([0-9]\+\) *\*.*/\1/p') # current workspace
-else
-  cw=$1
-fi
-
-wid=''
-lines=''
 while read -r line ; do
-  # echo $line
-  width=$(echo $line | awk '{ print $3 }')
-  top=$(echo $line | awk '{ print $4 }')
-  workspace=$(echo $line | awk '{ print $2 }')
-  if [ "$width" -gt "$bscreen" ]
+  workspace_monitor=$(echo $line | awk '{print $NF}' | grep 'm3')
+  if [ "$workspace_monitor" ]
   then
-    if [ "$width" -lt "$escreen" ]
-    then
-      if [ "$top" -gt "$topscreen" ]
-      then
-        if [ "$workspace" = "$cw" ] || [ "$cw" = "all" ]
-        then
-          if [ -z "$wid" ]
-          then
-            linne=$(echo $line | sed 's/[\]//g' | awk '{ s = ""; for (i = 9; i <= NF; i++) s = s $i " "; print $1, $7, s }')
-            windowid=$(echo $line | awk '{ print $1 }')
-            wwworkspace=$(echo $line | awk '{ print $2 }')
-            title=$(echo $line | sed 's/[\]//g' | awk '{ s = ""; for (i = 8; i <= NF; i++) s = s $i " "; print $7, s }')
-            linne="$windowid k$((wwworkspace+1)) $title"
-            lines="$linne\n$lines"
-
-            # old
-            # line=$(echo $line | sed 's/[\]//g' | awk '{ s = ""; for (i = 9; i <= NF; i++) s = s $i " "; print $1, $7, s }')
-            # lines="$line\n$lines"
-          fi
-        fi
-      fi
-    fi    
+    lines="$line\n$lines"
   fi
-done < <(wmctrl -dliGx)
+done < <(cat /home/danilo/scripts/dmenu/list_windows)
 
 choice=$(echo -e "$lines" | awk '{ s = ""; for (i = 2; i <= NF; i++) s = s $i " "; print s }' | dmenu -i -l 20 -b -m 0 -fn Monospace-16:normal)
 if [ "$choice" == "" ]; then
   exit
 fi
 
+choice=$(echo $choice | tr -d '\n')
 wid=$(echo -e "$lines" | grep "$choice" | awk '{ print $1 }')
-wworkspace=$(wmctrl -dliGx | grep "$wid" | awk '{ print $2 }')
-wmctrl -s $wworkspace && sleep 0.01s
+monitor_workspace=$(echo -e "$lines" | grep "$choice" | awk '{ print $NF }')
+workspace=$(expr ${monitor_workspace:1:1} - 1)
+
+wmctrl -s $workspace && sleep 0.01s
 wmctrl -i -a $wid
