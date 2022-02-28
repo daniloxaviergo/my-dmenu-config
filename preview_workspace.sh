2898a68961519f92a013ed9068825b69c2badcae
Danilo Xavier <daniloxaviergo@gmail.com>
2022-02-28 17:40:39 -0300

    add next workspace and preview

---
+++ preview_workspace.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+total=$(expr $(wmctrl -d | wc -l) - 1)
+current=$(wmctrl -d | sed -n 's/^\([0-9]\+\) *\*.*/\1/p')
+if [ -z "$total" ] || [ -z "$current" ]; then
+  echo 1>&2 "$0: Could not obtain workspace information!"
+  exit 2
+fi
+target=$(($current-1))
+if [ $target -eq -1 ]; then
+  target=$(($total-1))
+fi
+wmctrl -s $target
+
+sleep 0.1s
+
+/home/danilo/scripts/dmenu/dzen_current_window.sh &
